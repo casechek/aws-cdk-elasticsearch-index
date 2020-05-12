1287dec8fe433ecc8a9d2b820550aa0c8d3b1ca7
Nour Al-Habash <nour35000@gmail.com>
2020-05-12 00:01:16 -0500

    refactor: 💡 update log msgs and increase readability

--- src/on-event/on-event.ts
+++ src/on-event/on-event.ts
@@ -10,6 +10,41 @@ import { INDEX_NAME_KEY } from './constants';
 
 export class TimeoutError extends Error {}
 
+const getMappingFromBucket = async (
+  s3: S3,
+  bucketParams: S3.GetObjectRequest
+): Promise<string> => {
+  const s3ObjectResponse = await s3.getObject(bucketParams).promise();
+  return JSON.parse((s3ObjectResponse.Body as Buffer).toString());
+};
+
+const checkClusterHealth = async (
+  es: Client,
+  maxRetries = 10,
+  // tslint:disable-next-line:no-any
+  logger: (...value: any) => void
+) => {
+  let retryHealth = maxRetries;
+  while (retryHealth > 0) {
+    const response = await es.cluster.health(
+      {
+        wait_for_status: 'yellow',
+        timeout: '60s',
+      },
+      { maxRetries: 0 }
+    );
+    logger('received health response', response.body);
+    if (response.body.timed_out) {
+      retryHealth -= 1;
+      if (retryHealth === 0) {
+        throw new TimeoutError();
+      }
+    } else {
+      retryHealth = 0;
+    }
+  }
+};
+
 export function createHandler(params: {
   s3: S3;
   es: Client;
@@ -25,41 +60,17 @@ export function createHandler(params: {
     const log = params.logger?.log ?? mockLog; // two lines because PhpStorm doesn't quite handle ?? correctly in the case of a function as value
 
     if (event.RequestType === 'Create') {
-      log(event);
-      const s3ObjectResponse = await params.s3
-        .getObject({
-          Bucket: params.bucketName,
-          Key: params.objectKey,
-        })
-        .promise();
-
-      const mapping = JSON.parse((s3ObjectResponse.Body as Buffer).toString());
-      log('downloaded s3 object', mapping);
-
-      log('waiting for cluster to become healthy');
-      let retryHealth = params.maxHealthRetries ?? 10;
-      while (retryHealth > 0) {
-        const response = await params.es.cluster.health(
-          {
-            wait_for_status: 'yellow',
-            timeout: '60s',
-          },
-          { maxRetries: 0 }
-        );
-        log('received health response', response.body);
-        if (response.body.timed_out) {
-          retryHealth -= 1;
-          if (retryHealth === 0) {
-            throw new TimeoutError();
-          }
-        } else {
-          retryHealth = 0;
-        }
-      }
+      const mapping = await getMappingFromBucket(params.s3, {
+        Bucket: params.bucketName,
+        Key: params.objectKey,
+      });
+      log('Downloaded mapping from S3:', mapping);
+      log('Waiting for cluster to become healthy');
+      await checkClusterHealth(params.es, params.maxHealthRetries, log);
 
       const indexId = randomBytes(16).toString('hex');
       const indexName = `${params.indexNamePrefix}-${indexId}`;
-      log(`attempting to create index ${indexName}`);
+      log(`Attempting to create index ${indexName}`);
       const response = await params.es.indices.create(
         {
           index: indexName,
@@ -68,7 +79,7 @@ export function createHandler(params: {
         { requestTimeout: 120 * 1000, maxRetries: 0 }
       );
 
-      log('response from create index', response);
+      log('Response from create index:', response);
       return {
         PhysicalResourceId: indexId,
         Data: {
